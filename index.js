6cc3501884eeeb5732cdd595a3f1271fae0547bc
Your NameSailakshmy <you@example.comsailakshmys26@gmail.com>
2024-08-19 13:34:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// import json-server
+const jsonServer = require('json-server');
+
+// creater server using create() method 
+const mediaPlayerServer = jsonServer.create()
+
+// create a path/route to db.json file
+const router = jsonServer.router('db.json')
+
+//create a middle ware
+const middleware = jsonServer.defaults();
+
+// add middleware and router to mediaPlayerServer 
+
+
+mediaPlayerServer.use(middleware);
+mediaPlayerServer.use(router)
+
+const PORT = 4000;
+
+// run the mediaPlayerServer 
+mediaPlayerServer.listen(PORT,()=>{
+    console.log(`Mediaplayer server running on PORT ${PORT}`)
+})
